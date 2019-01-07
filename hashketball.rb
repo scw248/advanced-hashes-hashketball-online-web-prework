7bda565f7c40b235262ff44f115ebc6edbb3f1c9
Stephen Wright <scw248@gmail.com>
2019-01-07 02:50:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,6 +185,7 @@ def player_numbers(team_name)
    game_hash.each do |location, team|
     team.each do |category, values|
       if values = team_name 
+        
     team.each do |category, values|    
       if category == :players
         values.each do |player, stats| 
